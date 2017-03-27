b08c69abb1589bf5aa7fe5b7bfe60c74b6aada5a
Thiago Cury <thiagocury@gmail.com>
2017-03-27 15:57:48 -0300

    Exemplo Classe Pessoa com JMenuBar / JFrame e JInternalFrame

---
+++ ExPessoaComJMenuBar/src/modelo/Pessoa.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author QI
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", idade=" + idade + '}';
+    }
+}
